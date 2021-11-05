d9e4329aa9f4739337a4a0307d5f9f5c81802f20
Giedre <giedre.klima@gmail.com>
2021-11-05 23:35:17 +0200

    isolate routes for faster tests

--- nadia/app.test.js
+++ nadia/app.test.js
@@ -1,13 +1,14 @@
-const request = require('supertest');
+const request = require("supertest");
 
 let app;
 const mockMorgan = jest.fn((req, res, next) => next());
 
-describe('errors', () => {
+describe("errors", () => {
   const OLD_ENV = process.env;
 
   beforeAll(() => {
-    jest.mock('morgan', () => () => mockMorgan);
+    jest.mock("morgan", () => () => mockMorgan);
+    jest.mock("./routes", () => (req, res, next) => next());
   });
 
   beforeEach(() => {
@@ -16,25 +17,23 @@ describe('errors', () => {
   });
 
   afterAll(() => {
-    jest.unmock('morgan');
+    jest.unmock("morgan");
+    jest.unmock("./routes");
     process.env = OLD_ENV;
   });
 
+  it("should return a 404 for a missing page in test", async () => {
+    app = request(require("./app"));
+    const response = await app.get("/bananas").expect(404);
 
-  it('should return a 404 for a missing page in test', async () => {
-    app = request(require('./app'));
-    const response = await app.get('/bananas')
-      .expect(404);
-
-    expect(response.text).toContain('app.js');
+    expect(response.text).toContain("app.js");
   });
 
-  it('should return a 404 for a missing page in production', async () => {
-    process.env.NODE_ENV = 'production';
-    app = request(require('./app'));
-    const response = await app.get('/bananas')
-      .expect(404);
+  it("should return a 404 for a missing page in production", async () => {
+    process.env.NODE_ENV = "production";
+    app = request(require("./app"));
+    const response = await app.get("/bananas").expect(404);
 
-    expect(response.text).not.toContain('app.js');
+    expect(response.text).not.toContain("app.js");
   });
-})
+});
