3af905b1c806ebfc76eb12d365a39e70201151de
Giedre <giedre.klima@gmail.com>
2021-11-05 15:08:02 +0200

    mock entire module

--- nadia/lib/reservations.test.js
+++ nadia/lib/reservations.test.js
@@ -1,7 +1,28 @@
-const reservations = require("./reservations");
 const Reservation = require("./schema/reservation");
 
+describe("fetch", () => {
+  let reservations;
+  beforeAll(() => {
+    jest.mock("./reservations");
+    reservations = require("./reservations");
+  });
+
+  afterAll(() => {
+    jest.unmock("./reservations");
+  });
+
+  it("should be mocked and not create a databese record", () => {
+    expect(reservations.fetch()).toBeUndefined();
+  });
+});
+
 describe("validate", () => {
+  let reservations;
+
+  beforeAll(() => {
+    reservations = require("./reservations");
+  });
+
   it("should resolve with no optional fields", async () => {
     const reservation = new Reservation({
       date: "2017/06/10",
@@ -32,6 +53,12 @@ describe("validate", () => {
 });
 
 describe("create", () => {
+  let reservations;
+
+  beforeAll(() => {
+    reservations = require("./reservations");
+  });
+
   it("should reject if validation fails", async () => {
     // Store the original.
     const original = reservations.validate;
